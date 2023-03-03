6f23022fac31f92ef492ad5c8d22c4e3e88d0174
Oskar Wiktor <oskar.wiktor3@wp.pl>
2023-03-03 10:50:06 +0100

    abc

---
+++ 1-ts-purpose/index.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var axios_1 = require("axios");
+var url = 'https://jsonplaceholder.typicode.com/todos/1';
+axios_1["default"].get(url).then(function (respose) {
+    //Response.data has properties of:
+    //id
+    //title
+    //completed
+    console.log(respose.data);
+    var todo = respose.data;
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    console.log("\n    The Todo with ID: ".concat(id, "\n    Has a title of: ").concat(title, "\n    Is it finished? ").concat(completed, "\n    "));
+});
